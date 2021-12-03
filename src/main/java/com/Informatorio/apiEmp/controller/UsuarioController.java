db6be13aa9b3dbb9c13e380169d8d8e1b9d6c1b3
Carlos <hernandeasi7@gmail.com>
2021-12-03 00:50:11 -0300

    Usuario Put y Delete Agregado

--- src/main/java/com/Informatorio/apiEmp/controller/UsuarioController.java
+++ src/main/java/com/Informatorio/apiEmp/controller/UsuarioController.java
@@ -1,6 +1,5 @@
 package com.Informatorio.apiEmp.controller;
 
-
 import java.time.LocalDate;
 
 import javax.validation.Valid;
@@ -13,8 +12,11 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -46,5 +48,29 @@ public class UsuarioController {
     @PostMapping
     public ResponseEntity<?> crear(@RequestBody @Valid Usuario usuario) {
         return new ResponseEntity<>(usuarioRepository.save(usuario), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> modificarUsuario(
+        @PathVariable("id") Long id,
+        @Valid @RequestBody Usuario usuario) {
+            Usuario usuarioExistente = usuarioRepository.findById(id).get();
+            usuarioExistente.setNombre(usuario.getNombre());
+            usuarioExistente.setApellido(usuario.getApellido());
+            usuarioExistente.setEmail(usuario.getEmail());
+            usuarioExistente.setPassword(usuario.getPassword());
+            usuarioExistente.setCiudad(usuario.getCiudad());
+            usuarioExistente.setProvincia(usuario.getProvincia());
+            usuarioExistente.setPais(usuario.getPais());
+            return new ResponseEntity<>(usuarioRepository.save(usuarioExistente), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Long> deletePost(@PathVariable Long id) {
+        if (usuarioRepository.findById(id).isPresent()) {
+            usuarioRepository.deleteById(id);
+            return new ResponseEntity<>(id, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
